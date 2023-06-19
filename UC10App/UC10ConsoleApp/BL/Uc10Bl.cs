2f73f422c18e6fff9529e6a2d72d17b868117af0
Yevhen Shchukin <eschuk@softserveinc.com>
2023-06-19 18:32:22 +0300

    UC#10 Implement regex

---
+++ UC10App/UC10ConsoleApp/BL/Uc10Bl.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC10ConsoleApp.BL
+{
+	public class Uc10Bl
+	{
+		public bool RegExTest(string inputString, int inputStringLength)
+		{
+			var patternFormat = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!\"#$%&'()*+,\\-./:;<=>?@[\\\\\\]^_`{{|}}~])(?!.*[\\s])(.{{1,{0}}})$";
+
+			var pattern = string.Format(patternFormat, inputStringLength.ToString());
+
+			var rg = new Regex(pattern);
+
+			return rg.IsMatch(inputString);
+		}
+
+	}
+}
---
+++ UC10App/UC10ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UC10ConsoleApp.BL;
+
+Console.WriteLine("UC10 - START");
+
+var ucBl = new Uc10Bl();
+
+var inputString = "Lore7$Psum";
+
+var result = ucBl.RegExTest(inputString, 10);
+
+Console.WriteLine($"Result: {result}");
+
